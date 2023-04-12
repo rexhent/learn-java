c7056575cd82ecfb30d296bfa1be9292ed4dec43
rexhent <dhannah10@outlook.com>
2023-04-12 13:19:37 +1000

    first

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,49 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Dominic");
+        System.out.println("Hello World");
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens!");
+        }
+
+        int topScore = 90;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
+            System.out.println("Greater than the second top score and less than 100.");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true.");
+        }
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is true");
+        }
+        boolean isCar = false;
+        if (!isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+        String makeOfCar = "Volkswagen";
+        boolean isDomestic = (makeOfCar == "Volkswagen") ? false : true;
+        if (isDomestic) {
+            System.out.println("This car is domestic to our country");
+        }
+        String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
+        System.out.println(s);
+
+        double firstDouble = 20.00;
+        double secondDouble = 80.00;
+        double thirdDouble = (firstDouble + secondDouble) * 100.00;
+        double forthDouble = thirdDouble % 40.00;
+        boolean remainderIsZero = forthDouble == 0;
+        System.out.println(forthDouble);
+        if (!remainderIsZero) System.out.println("got some remainder");
+    }
+}
---
+++ kotlin1/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val name = "Dom"
+    println(name)
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+}
